bcd9d283acbfcd4dd1f6fb02b38e271caab2044a
toshinori.sato <tyahhahha@gmail.com>
2023-11-13 21:34:25 +0900

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+/// 引数同士を足す
+///
+/// # Example
+///
+/// ```
+/// let five = 5;
+/// let six = 6;
+///
+/// assert_eq!(my_crate::add(five, six), 11);
+/// ```
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
